7f37778972cea22ee4457300f0c7a1a5fce70e10
ub-victor <victoireushindi371@gmail.com>
2025-09-07 01:58:59 +0200

    A simple react project

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,26 @@ export default function App() {
         {id: crypto.randomUUID(), title: newItem, completed: false},
 
       ]
-    })
+    })    
 
     setNewItem('')
+  }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos =>{
+      return currentTodos.map(todo =>{
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+        return todo;
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos =>{
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
   return (
     <>
@@ -37,10 +54,17 @@ export default function App() {
           return (
             <li key = {todo.id}>
               <label>
-                <input type="checkbox" checked = {todo.completed} />
+                <input 
+                  type="checkbox" 
+                  checked = {todo.completed}
+                onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
               </label>
-          <button className="btn btn-danger">Delete</button>
+          <button
+            
+            className="btn btn-danger"
+          >Delete</button>
         </li>
           )
         })}
