6052081ee636e634b37f17fde7542b84187f208c
ub-victor <victoireushindi371@gmail.com>
2025-09-07 00:28:30 +0200

    A simple react project

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,24 @@ import "./styles.css";
 
 export default function App() {
   const [newItem, setNewItem] = useState('')
+  const [todos, setTodos] = useState ([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newItem, completed: false},
+
+      ]
+    })
+
+    setNewItem('')
+  }
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
           <div className="form-row">
             <label htmlFor="item">New Item</label>
             <input 
@@ -18,20 +33,17 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
+        {todos.map(todo => {
+          return (
+            <li key = {todo.id}>
+              <label>
+                <input type="checkbox" checked = {todo.completed} />
+                {todo.title}
+              </label>
           <button className="btn btn-danger">Delete</button>
         </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+          )
+        })}
       </ul>
 
     </>
